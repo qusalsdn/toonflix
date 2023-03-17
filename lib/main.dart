3b21cc3d082c0e3ebb8ae62ee99b9e0ad67c2f74
변민우 <yja05092@gmail.com>
2023-03-17 15:46:36 +0900

    StatelessWidget
    StatelessWidget은 아주 기초적인 Widget이고 화면에 뭔가를 띄워주는 거 말고는 다른 기능이 없다.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 
 // flutter는 widget의 조합이다.
 // build 메소드는 widget의 ui를 만든다.
+// StatelessWidget은 아주 기초적인 Widget이고 화면에 뭔가를 띄워주는 거 말고는 다른 기능이 없다.
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
