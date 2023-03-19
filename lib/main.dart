77fd5d38f7da1b51c436a0199bb3edd855469206
변민우 <yja05092@gmail.com>
2023-03-19 21:38:46 +0900

    VSCode Settings
    Open User Settings(JSON)에서 "editor.codeActionsOnSave": { "source.fixAll": true }을 입력해 주면 파일을 저장하는 순간 const를 해줘야 하는 부분들을 자동으로 기입해 준다.
    또한 "dart.previewFlutterUiGuides": true를 입력해 주면 code에서 각각의 연관관계를 연결시켜주어 화면으로 가이드라인을 보여준다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 // flutter는 widget의 조합이다.
 // build 메소드는 widget의 ui를 만든다.
 // StatelessWidget은 아주 기초적인 Widget이고 화면에 뭔가를 띄워주는 거 말고는 다른 기능이 없다.
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     // Material은 구글, Cupertino는 ios 스타일이다.
     return MaterialApp(
       // Scaffold는 뼈대이다.
       home: Scaffold(
-          backgroundColor:
-              Color(0xFF181818), // 0xFF 뒤에 색상값을 붙이면 custom으로 색상을 지정할 수 있다.
+          backgroundColor: const Color(
+              0xFF181818), // 0xFF 뒤에 색상값을 붙이면 custom으로 색상을 지정할 수 있다.
           body: Padding(
             // EdgeInsets.all()은 좌우상하 전부에 padding을 준다.
             // symmetric은 수평 or 수직에 padding을 줄 수 있다. horizontal: 수평 vertical: 수직
-            padding: EdgeInsets.symmetric(horizontal: 20),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 80,
                 ),
                 Row(
@@ -34,7 +36,7 @@ class App extends StatelessWidget {
                       crossAxisAlignment:
                           CrossAxisAlignment.end, // crossAxisAlignment은 수직 방향
                       children: [
-                        Text(
+                        const Text(
                           'Hey, Minu',
                           style: TextStyle(
                               color: Colors.white,
@@ -52,7 +54,7 @@ class App extends StatelessWidget {
                     )
                   ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 120,
                 ),
                 Text(
@@ -62,8 +64,8 @@ class App extends StatelessWidget {
                     fontSize: 22,
                   ),
                 ),
-                SizedBox(height: 5),
-                Text(
+                const SizedBox(height: 5),
+                const Text(
                   '\$5 194 482',
                   style: TextStyle(
                     color: Colors.white,
@@ -71,7 +73,7 @@ class App extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 30,
                 ),
                 Row(
@@ -79,10 +81,10 @@ class App extends StatelessWidget {
                     // Container는 HTML에서 div 같은 것이다.
                     Container(
                       decoration: BoxDecoration(
-                        color: Color(0xFFF2B33A),
+                        color: const Color(0xFFF2B33A),
                         borderRadius: BorderRadius.circular(45),
                       ),
-                      child: Padding(
+                      child: const Padding(
                           padding: EdgeInsets.symmetric(
                             vertical: 20,
                             horizontal: 50,
