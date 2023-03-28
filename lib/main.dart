085cbdd0600f008c6069163da940aba9f13ebbb5
변민우 <yja05092@gmail.com>
2023-03-28 16:48:31 +0900

    UI 연습
    Reusable Cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/widgets/Button.dart';
+import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
   runApp(const App());
@@ -19,170 +20,138 @@ class App extends StatelessWidget {
       home: Scaffold(
           backgroundColor: const Color(
               0xFF181818), // 0xFF 뒤에 색상값을 붙이면 custom으로 색상을 지정할 수 있다.
-          body: Padding(
-            // EdgeInsets.all()은 좌우상하 전부에 padding을 준다.
-            // symmetric은 수평 or 수직에 padding을 줄 수 있다. horizontal: 수평 vertical: 수직
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  mainAxisAlignment:
-                      MainAxisAlignment.end, // mainAxisAlignment은 수평 방향
-                  children: [
-                    Column(
-                      crossAxisAlignment:
-                          CrossAxisAlignment.end, // crossAxisAlignment은 수직 방향
-                      children: [
-                        const Text(
-                          'Hey, Minu',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.bold),
+          // SingleChildScrollView로 인해 화면에서 콘텐츠가 넘쳐도 스크롤로 내리거나 올려서 볼 수 있다.
+          body: SingleChildScrollView(
+            child: Padding(
+              // EdgeInsets.all()은 좌우상하 전부에 padding을 준다.
+              // symmetric은 수평 or 수직에 padding을 줄 수 있다. horizontal: 수평 vertical: 수직
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    mainAxisAlignment:
+                        MainAxisAlignment.end, // mainAxisAlignment은 수평 방향
+                    children: [
+                      Column(
+                        crossAxisAlignment:
+                            CrossAxisAlignment.end, // crossAxisAlignment은 수직 방향
+                        children: [
+                          const Text(
+                            'Hey, Minu',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Welcome back',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Total Balance',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 22,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text(
+                    '\$5 194 482',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 42,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Container는 HTML에서 div 같은 것이다.
+                      Button(
+                        text: 'Transfer',
+                        bgColor: Color(0xFFF2B33A),
+                        textColor: Colors.black,
+                      ),
+                      Button(
+                        text: 'Request',
+                        bgColor: Color(0xFF1F2123),
+                        textColor: Colors.white,
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Wallets',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          'Welcome back',
+                      ),
+                      Text('View All',
                           style: TextStyle(
                             color: Colors.white.withOpacity(0.8),
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Text(
-                  'Total Balance',
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.8),
-                    fontSize: 22,
+                            fontSize: 18,
+                          )),
+                    ],
                   ),
-                ),
-                const SizedBox(height: 5),
-                const Text(
-                  '\$5 194 482',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 42,
-                    fontWeight: FontWeight.bold,
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // Container는 HTML에서 div 같은 것이다.
-                    Button(
-                      text: 'Transfer',
-                      bgColor: Color(0xFFF2B33A),
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      text: 'Request',
-                      bgColor: Color(0xFF1F2123),
-                      textColor: Colors.white,
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 100,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Wallets',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text('View All',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.8),
-                          fontSize: 18,
-                        )),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  // Clip.hardEdge로 인해 Container 레이아웃을 벗어나게 되면 짤리게 된다.
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF1F2123),
-                    borderRadius: BorderRadius.circular(20),
+                  const CurrencyCard(
+                    name: 'Euro',
+                    code: 'EUR',
+                    amount: '6 428',
+                    icon: Icons.euro_rounded,
+                    isInverted: false,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'Euro',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 15,
-                            ),
-                            Row(
-                              children: [
-                                const Text(
-                                  '6 428',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  width: 5,
-                                ),
-                                Text(
-                                  'EUR',
-                                  style: TextStyle(
-                                    color: Colors.white.withOpacity(0.7),
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Transform.scale(
-                          scale: 2.2, // 크기를 2.2배 키운다.
-                          child: Transform.translate(
-                            offset: const Offset(-7, 7), // x, y 축으로 이동시킬 수 있다.
-                            child: const Icon(
-                              Icons.euro_rounded,
-                              color: Colors.white,
-                              size: 88,
-                            ),
-                          ),
-                        ),
-                      ],
+                  Transform.translate(
+                    offset: const Offset(0, -15),
+                    child: const CurrencyCard(
+                      name: 'Bitcoin',
+                      code: 'BTC',
+                      amount: '9 785',
+                      icon: Icons.currency_bitcoin,
+                      isInverted: true,
                     ),
                   ),
-                ),
-              ],
+                  Transform.translate(
+                    offset: const Offset(0, -35),
+                    child: const CurrencyCard(
+                      name: 'Dollar',
+                      code: 'USD',
+                      amount: '428',
+                      icon: Icons.attach_money_outlined,
+                      isInverted: false,
+                    ),
+                  )
+                ],
+              ),
             ),
           )),
     );
