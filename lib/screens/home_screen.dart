0db03b4b044f6e27b0823abf93f7909ffb3bd92d
변민우 <yja05092@gmail.com>
2023-03-30 22:21:19 +0900

    Webtoon Card

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,19 +30,12 @@ class HomeScreen extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             // ListView.builder는 모든 아이템을 한 번에 만들지 않고 만들려는 아이템에 itemBuilder 함수를 실행한다.
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              // separatorBuilder는 위젯을 리턴하는데 그 위젯은 리스트 아이템 사이에 렌더링이된다.
-              // 아이템들을 구분하기 위해 사용한다.
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 20,
+            return Column(children: [
+              const SizedBox(height: 50),
+              Expanded(
+                child: makeList(snapshot),
               ),
-            );
+            ]);
           }
           return const Center(
             // CircularProgressIndicator는 loading indicator를 보여준다.
@@ -50,6 +43,55 @@ class HomeScreen extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 15,
+                        offset: const Offset(10, 10),
+                        color: Colors.black.withOpacity(0.5))
+                  ]),
+              clipBehavior: Clip.hardEdge,
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ],
+        );
+      },
+      // separatorBuilder는 위젯을 리턴하는데 그 위젯은 리스트 아이템 사이에 렌더링이된다.
+      // 아이템들을 구분하기 위해 사용한다.
+      separatorBuilder: (context, index) => const SizedBox(width: 20),
     );
   }
 }
