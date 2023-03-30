03f62e0e86b4572b7e4dd4dd1445d3dc90540b59
변민우 <yja05092@gmail.com>
2023-03-30 20:17:51 +0900

    fromJson

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,24 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = 'today';
 
-  void getTodaysToons() async {
+  // 함수가 비동기 함수이고 리턴값이 있다면 Future로 타입을 지정해줘야 한다.
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     // Future는 미래에 받을 값의 타입을 알려준다.
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      print(response.body);
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     } else {
       throw Error();
     }
